2b265fc7010be240003c427254b9d5181c37217c
Alex <515260@camosun>
2025-03-13 09:45:08 -0700

    Added basic hit detection and removed gravity

--- ActionGame2025/GameScene.swift
+++ ActionGame2025/GameScene.swift
@@ -8,14 +8,17 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
 
     var sprite: SKSpriteNode!
+    
+    let spriteCategory1: UInt32 = 0b1
+    let spriteCategory2: UInt32 = 0b10
     
     override func didMove(to view: SKView) {
         sprite = SKSpriteNode(imageNamed: "PlayerSprite")
         sprite.position = CGPoint(x: size.width / 2, y: size.height / 2)
-        sprite.size = CGSize(width: 75, height: 50)
+        sprite.size = CGSize(width: 50, height: 50)
         addChild(sprite)
         
         let opponentSprite = SKSpriteNode(imageNamed: "OpponentSprite")
@@ -26,6 +29,16 @@ class GameScene: SKScene {
         let upMovement = SKAction.move(to: CGPoint(x: size.width / 2, y: size.height), duration: 1)
         let movement = SKAction.sequence([downMovement, upMovement])
         opponentSprite.run(SKAction.repeatForever(movement))
+        
+        sprite.physicsBody = SKPhysicsBody(circleOfRadius: 50)
+        opponentSprite.physicsBody = SKPhysicsBody(circleOfRadius: 50)
+        sprite.physicsBody?.categoryBitMask = spriteCategory1
+        sprite.physicsBody?.contactTestBitMask = spriteCategory1
+        sprite.physicsBody?.collisionBitMask = spriteCategory1
+        opponentSprite.physicsBody?.categoryBitMask = spriteCategory1
+        opponentSprite.physicsBody?.contactTestBitMask = spriteCategory1
+        opponentSprite.physicsBody?.collisionBitMask = spriteCategory1
+        self.physicsWorld.contactDelegate = self
     }
     
     func touchDown(atPoint pos : CGPoint) {
@@ -36,6 +49,10 @@ class GameScene: SKScene {
     
     func touchUp(atPoint pos : CGPoint) {
         sprite.run(SKAction.move(to: pos, duration: 1))
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        print("Hit!")
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
